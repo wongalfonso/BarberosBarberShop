b078276daa86a9cdc4287191c61aa963dc85af41
wongalofonso <alfonsowongsd@gmail.com>
2018-08-02 16:50:27 -0700

    cleaned up working files

--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
-  res.status(200).send('ok');
-})
+// app.get('/', (req, res) => {
+//   res.status(200).send('ok');
+// })
 
+// app.get('*', (req,res) => {
+//   res.sendFile(path.join(__dirname, '../dist/index.html'));
+// });
 
 module.exports = app;
---
+++ src/app/js/app.main.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '../html/app.main.html',
+  styleUrls: ['../css/app.main.css']
+})
+
+export class AppComponent {}
