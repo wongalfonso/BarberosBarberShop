2be4d41813694815b88a5a2d827b62cade2a5fb1
wongalofonso <alfonsowongsd@gmail.com>
2018-08-02 15:05:47 -0700

    added express server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
 const path = require('path');
+const morgan = require('morgan');
+const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get('/', (req, res) => {
+  res.status(200).send('ok');
+})
 
 
 module.exports = app;
