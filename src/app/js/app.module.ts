27c8ee59a6fbbcd895da530e8f1dedba657d71aa
wongalofonso <alfonsowongsd@gmail.com>
2018-08-09 15:18:50 -0700

    fixed screen settings

--- src/app/js/app.module.ts
+++ src/app/js/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.main';
 import { Carousel } from './app.carousel';
 import { AppInfo } from './app.info';
+import { Navbar } from './app.navbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     Carousel,
-    AppInfo
+    AppInfo,
+    Navbar
   ],
   imports: [
     BrowserModule,
---
+++ src/app/js/app.navbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: '../html/app.navbar.html',
+  styleUrls: ['../css/app.navbar.scss']
+})
+
+export class Navbar {}
